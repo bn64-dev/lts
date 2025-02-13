1c07c41f2af71fc45a174b44362dac54b0cd2a6e
BN64-commits <bla@plexure.net>
2025-02-13 06:17:20 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function makeColourValue() {
+	return Math.round(Math.random() * 255);
+}
+
+function setButtonColour(button, red, green, blue) {
+	button.setAttribute('style',
+		'background-color : rgb(' + red + ',' + green + ',' + blue + ');'
+	);
+}
+let buttons = document.getElementsByClassName('colourButton');
+let heading = document.getElementById('colourValue');
+let answerMessage = document.getElementById('answer');
+let answerButton = Math.round(Math.random() * (buttons.length - 1));
+let resetbutton = document.getElementById('resetButton')
+
+function startGame() {
+	answerMessage.innerHTML = "";
+	let answerButton = Math.round(Math.random() * (buttons.length - 1));
+	for (let i = 0; i < buttons.length; i++) {
+		let red = makeColourValue();
+		let green = makeColourValue();
+		let blue = makeColourValue();
+		setButtonColour(buttons[i], red, green, blue);
+		if (i === answerButton) {
+			heading.innerHTML = `(${red}, ${green}, ${blue})`;
+		}
+
+		buttons[i].addEventListener('click', function() {
+			if (this === buttons[answerButton]) {
+				answerMessage.innerHTML = "Correct";
+			} else {
+				answerMessage.innerHTML = "Wrong answer! Guess again!";
+			}
+		});
+	}
+}
+startGame();
+resetButton.addEventListener('click', startGame);
